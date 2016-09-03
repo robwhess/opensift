59f8e6f10be3be4c196252b3ce26dcbd644c16e4
Jeff 'Raid' Baitis <jeff@baitis.net>
2016-09-03 12:18:07 -0700

    Multithreaded image loading and feature extraction

--- src/match_num.c
+++ src/match_num.c
@@ -17,7 +17,9 @@
 #include <highgui.h>
 
 #include <stdio.h>
+#include <pthread.h>
 
+#define DEBUG 1
 
 /* the maximum number of keypoint NN candidates to check during BBF search */
 #define KDTREE_BBF_MAX_NN_CHKS 200
@@ -25,47 +27,90 @@
 /* threshold on squared ratio of distances between NN and 2nd NN */
 #define NN_SQ_DIST_RATIO_THR 0.49
 
+struct thread_data {
+  int             thread_id;
+  char*           filename;
+  int             num_features;
+  struct feature* features;
+};
 
-int main( int argc, char** argv )
-{
-  IplImage* img1, * img2;
-  struct feature* feat1, * feat2, * feat;
+void* process_image(void* arg) {
+  int n;
+  struct thread_data* ctx;
+  IplImage*       img;
+
+  ctx = (struct thread_data*)arg;
+  img = cvLoadImage(ctx->filename, 1);
+  if (!img) fatal_error("Unable to load image from %s", ctx->filename);
+  ctx->num_features = sift_features(img, &(ctx->features));
+  if (DEBUG)
+  fprintf(stderr, "Found %d features in %s...\n", ctx->num_features, ctx->filename);
+  cvReleaseImage(&img);
+  pthread_exit(NULL);
+}
+
+int main( int argc, char** argv ) {
   struct feature** nbrs;
   struct kd_node* kd_root;
   CvPoint pt1, pt2;
   double d0, d1;
-  int n1, n2, k, i, m = 0;
+  int i, num_matches = 0;
+  pthread_t threads[2];
+  struct thread_data td[2];
+  pthread_attr_t attr;
+  void* status;
+  
 
+
+  /* Handle filenames */
   if( argc != 3 )
     fatal_error( "usage: %s <img1> <img2>", argv[0] );
-  
-  img1 = cvLoadImage( argv[1], 1 );
-  if( ! img1 )
-    fatal_error( "unable to load image from %s", argv[1] );
-  img2 = cvLoadImage( argv[2], 1 );
-  if( ! img2 )
-    fatal_error( "unable to load image from %s", argv[2] );
 
-  n1 = sift_features( img1, &feat1 );
-  n2 = sift_features( img2, &feat2 );
-  kd_root = kdtree_build( feat2, n2 );
-  for( i = 0; i < n1; i++ ) {
-    feat = feat1 + i;
+  td[0].filename=argv[1];
+  td[1].filename=argv[2];
+
+
+  /* Launch processing threads */
+
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+  for (i=0; i < 2; ++i) { 
+    int rc;
+    rc = pthread_create(&threads[i], &attr, process_image, (void*)&td[i]);
+    if (rc) fatal_error("Unable to create processing thread (err %d).",rc);
+  }
+
+  pthread_attr_destroy(&attr);
+
+  /* Wait for threads */
+
+  for (i=0; i < 2; ++i) {
+    int rc;
+    rc = pthread_join(threads[i], &status);
+    if (rc) fatal_error("Return code from thread pthread_join is %d",rc);
+  }
+
+  kd_root = kdtree_build(td[1].features, td[1].num_features);
+
+  for(i = 0; i < td[0].num_features; ++i) {
+    int k;
+    struct feature* feat;
+    feat = td[0].features + i;
     k = kdtree_bbf_knn( kd_root, feat, 2, &nbrs, KDTREE_BBF_MAX_NN_CHKS );
     if( k == 2 ) {
       d0 = descr_dist_sq( feat, nbrs[0] );
       d1 = descr_dist_sq( feat, nbrs[1] );
-      if( d0 < d1 * NN_SQ_DIST_RATIO_THR ) m++;
+      if( d0 < d1 * NN_SQ_DIST_RATIO_THR ) ++num_matches;
     }
     free( nbrs );
   }
 
-  fprintf( stdout, "%d\n", m);
+  fprintf( stdout, "%d\n", num_matches);
 
-  cvReleaseImage( &img1 );
-  cvReleaseImage( &img2 );
   kdtree_release( kd_root );
-  free( feat1 );
-  free( feat2 );
+  /* Release structures */
+  for (i=0; i<2; ++i) free(td[i].features);
+  pthread_exit(NULL);
   return 0;
 }
